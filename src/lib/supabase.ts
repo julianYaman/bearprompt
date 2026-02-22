d54ca79b09e96ad65db2db3fbc5233d0a9ecce9e
Julian <mail@yaman.pro>
2026-02-22 22:07:25 +0100

    fix(db): fixed environment var issue

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import { env } from '$env/dynamic/public';
+import { env } from '$env/dynamic/private';
 
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
